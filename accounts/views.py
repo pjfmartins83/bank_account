8e95516a3512c442bc3fcd6d6b8448ace02e91dd
guilhermegouw <guilherme.gouw@gmail.com>
2024-06-28 10:47:11 -0300

    add bank account on user create.

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,15 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
+from .models import BankAccount
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        BankAccount.objects.create(owner=self.object, balance=0)
+        return response
